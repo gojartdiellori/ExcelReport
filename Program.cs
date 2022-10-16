20c04e47ed85fa894e34934d99235c8d627ec88d
Gojart <gojarti_369@hotmail.com>
2022-10-16 09:45:23 +0200

    Pushing project to Github

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Microsoft.Office.Interop.Excel;
+
+var persons=new List<Person>{
+    new Person {
+        ID = 1,
+        Name = "John"
+    },
+    new Person {
+        ID =2,
+        Name="Susane"
+    }
+};
+DisplayInExcel(persons);
+
+
+static void DisplayInExcel(IEnumerable<Person> persons)
+{
+    var excelApp = new Application();
+    excelApp.Visible = true;
+
+    Workbook workbook = excelApp.Workbooks.Add();
+
+    Worksheet workSheet = (Worksheet)workbook.Worksheets[1];
+
+    workSheet.Cells[1, "A"] = "ID Number";
+    workSheet.Cells[1, "B"] = "Name";
+
+    var row = 1;
+    foreach (var person in persons)
+    {
+        row++;
+        workSheet.Cells[row, "A"] = person.ID;
+        workSheet.Cells[row, "B"] = person.Name;
+    }
+
+    workbook.SaveAs(@"Test.xls");
+    
+}
